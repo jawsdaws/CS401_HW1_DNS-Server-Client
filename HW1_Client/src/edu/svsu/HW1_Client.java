1ca03617f7cdd554eeb4981720037ce1a38df041
Jamie Daws <jdelectronics1@gmail.com>
2017-08-31 22:07:45 -0400

    Finally have the server - client connections working with threads

--- HW1_Client/src/edu/svsu/HW1_Client.java
+++ HW1_Client/src/edu/svsu/HW1_Client.java
@@ -9,11 +9,7 @@ import javafx.scene.control.TextArea;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 
 public class HW1_Client extends Application {
@@ -21,13 +17,14 @@ public class HW1_Client extends Application {
     private VBox vbox;
     private Scene scene;
     private TextArea taDisplay;
-    private String hostname;
+    private Socket socket;
+    private DataInputStream inputFromServer;
+    private DataOutputStream outputToServer;
 
     /**
      * Default constructor.
      */
     public HW1_Client() {
-        hostname = "localhost";
         vbox = new VBox();
         scene = new Scene(vbox, 500, 500);
         taDisplay = new TextArea();
@@ -39,8 +36,7 @@ public class HW1_Client extends Application {
         taDisplay.setFont(Font.font("Courier New", 16));
         taDisplay.setPadding(new Insets(10, 10, 10, 10));
         taDisplay.setEditable(false);
-        taDisplay.setText("Welcome to the DNS Server." + "\n");
-        taDisplay.appendText("The hostname is " + hostname + "\n");
+        taDisplay.setText("Welcome to the DNS Client." + "\n");
 
         vbox.setStyle("-fx-border-color: black");
         vbox.getChildren().add(taDisplay);
@@ -50,59 +46,8 @@ public class HW1_Client extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        server();
-
-    }
-
-    private void server() {
-
-        InetAddress address = null;
-        try {
-            address = InetAddress.getByName(hostname);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        }
-        taDisplay.appendText("The ip is " + address.getHostAddress() + "\n");
-        SocketAcceptLoop sal = new SocketAcceptLoop();
-        Thread thread = new Thread(sal);
-        thread.start();
-    }
-
-    class SocketAcceptLoop implements Runnable {
-
-        @Override
-        public void run() {
-            taDisplay.appendText("Listening for clients.\n");
-            while (true) {
-                try {
-                    ServerSocket serverSocket = new ServerSocket(8000);
-                    Socket socket = serverSocket.accept();
-                    ConnectionHandle connectionhandle = new ConnectionHandle(socket);
-                    Thread thread = new Thread(connectionhandle);
-                    thread.start();
-                } catch (Exception E) {
-
-                }
-            }
-        }
-    }
-
-    class ConnectionHandle implements Runnable {
-
-        private Socket socket;
-
-        private ConnectionHandle(Socket socket) {
-            this.socket = socket;
-        }
-
-        @Override
-        public void run() {
-            try {
-                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
-                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        socket = new Socket("localhost", 8019);
+        inputFromServer = new DataInputStream(socket.getInputStream());
+        outputToServer = new DataOutputStream(socket.getOutputStream());
     }
 }
--- HW1_Server/src/edu/svsu/HW1_Server.java
+++ HW1_Server/src/edu/svsu/HW1_Server.java
@@ -1,8 +1,73 @@
 package edu.svsu;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class HW1_Server {
 
     public static void main(String[] args) {
-	    System.out.println("FUCK");// write your code here
+
+        InetAddress address = null;
+        String hostname = "localhost";
+        System.out.println("Welcome to the DNS server.");
+        System.out.println("The hostname is " + hostname);
+
+        try {
+            address = InetAddress.getByName(hostname);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("The ip is " + address.getHostAddress());
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(8019);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while (true) {
+            System.out.println("Listening for clients.");
+
+
+            try {
+
+                Socket socket = serverSocket.accept();
+                ConnectionHandle connectionhandle = new ConnectionHandle(socket);
+                Thread thread = new Thread(connectionhandle);
+                thread.start();
+            } catch (Exception E) {
+                //E.printStackTrace();
+            }
+        }
+    }
+
+    static class ConnectionHandle implements Runnable {
+
+        private Socket socket;
+
+        private ConnectionHandle(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("Connection.");
+            try {
+                DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+                //while(true){
+                    //System.out.println("FUCK");
+                //}
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
     }
 }
