cee2d2ae8c276eca82e41b122c23ea024d371964
Jamie Daws <jdelectronics1@gmail.com>
2017-09-05 20:15:46 -0400

    Added much of the file read and write stuff...

--- HW1_Server/src/edu/svsu/HW1_Server.java
+++ HW1_Server/src/edu/svsu/HW1_Server.java
@@ -1,9 +1,6 @@
 package edu.svsu;
 
-import java.io.EOFException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -21,6 +18,7 @@ public class HW1_Server {
         ServerSocket serverSocket = null;
         int port = 8019;
         String hostname = "localhost";
+        processFile(0);
 
         System.out.println("Welcome to the DNS server.");
         System.out.println("The hostname is " + hostname);
@@ -43,7 +41,7 @@ public class HW1_Server {
             System.out.println("Listening for clients.");
             try {
                 Socket socket = serverSocket.accept();
-                System.out.println("Client connected");
+                processFile(1);
                 ConnectionHandle connectionhandle = new ConnectionHandle(socket);
                 Thread thread = new Thread(connectionhandle);
                 thread.start();
@@ -53,7 +51,7 @@ public class HW1_Server {
         }
     }
 
-    static class ConnectionHandle implements Runnable {
+    private static class ConnectionHandle implements Runnable {
 
         private Socket socket;
 
@@ -87,6 +85,47 @@ public class HW1_Server {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    /**
+     * This function reads the count file and updates the count based on the int param.
+     * @param count The amount to add to the count read from the file.
+     *            Use -1 will decrement.
+     */
+    private static void processFile(int count) {
+
+        File file = new File("countfile.txt");
+
+        if (count == 0) {
+            try {
+                file.createNewFile();
+                BufferedWriter bw;
+                FileWriter fw;
+                fw = new FileWriter(file);
+                bw = new BufferedWriter(fw);
+                bw.write("0");
+                bw.close();
+            } catch (IOException e) {
+
+            }
+        } else {
+            try {
+                BufferedReader br;
+                br = new BufferedReader(new FileReader(file));
+                String line = br.readLine();
+                br.close();
+                BufferedWriter bw;
+                FileWriter fw;
+                fw = new FileWriter(file);
+                bw = new BufferedWriter(fw);
+                bw.write(Integer.toString(Integer.parseInt(line) + count));
+                bw.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
